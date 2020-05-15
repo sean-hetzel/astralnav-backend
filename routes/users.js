2f65a17b4b1804332bbdadccc103c4047ee54c57
Seanhetzel <seanhetzel1@gmail.com>
2020-05-14 21:42:12 -0700

    got basic user routes working with bt code

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,70 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const uuid = require('uuid');
+const users = require("../Users");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with users');
+const idFilter = req => user => user.id === parseInt(req.params.id);
+
+// Gets All users
+router.get("/", (req, res) => res.json(users));
+
+// Get Single User
+router.get("/:id", (req, res) => {
+  const found = users.some(idFilter(req));
+
+  if (found) {
+    res.json(users.filter(idFilter(req)));
+  } else {
+    res.status(400).json({ msg: `No user with the id of ${req.params.id}` });
+  }
+});
+
+// Create User
+router.post("/", (req, res) => {
+  const newUser = {
+    ...req.body,
+    id: uuid.v4(),
+    status: "active",
+  };
+
+  if (!newUser.name || !newUser.email) {
+    return res.status(400).json({ msg: "Please include a name and email" });
+  }
+
+  users.push(newUser);
+  res.json(users);
+  // res.redirect('/');
+});
+
+// Update User
+router.put("/:id", (req, res) => {
+  const found = users.some(idFilter(req));
+
+  if (found) {
+    users.forEach((user, i) => {
+      if (idFilter(req)(user)) {
+        const updUser = { ...user, ...req.body };
+        users[i] = updUser;
+        res.json({ msg: "User updated", updUser });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No user with the id of ${req.params.id}` });
+  }
+});
+
+// Delete User
+router.delete("/:id", (req, res) => {
+  const found = users.some(idFilter(req));
+
+  if (found) {
+    res.json({
+      msg: "User deleted",
+      users: users.filter((user) => !idFilter(req)(user)),
+    });
+  } else {
+    res.status(400).json({ msg: `No user with the id of ${req.params.id}` });
+  }
 });
 
 module.exports = router;
